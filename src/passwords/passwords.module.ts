cf1693d9ce69699efc92fc80f5279f5a8fec541d
Abdullah Omar <abdullah.aboode.omar@gmail.com>
2025-03-10 03:34:39 +0300

    imported the auth module

--- src/passwords/passwords.module.ts
+++ src/passwords/passwords.module.ts
@@ -4,9 +4,10 @@ import { PasswordsService } from './passwords.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Passwords } from './passwords.entity';
 import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Passwords]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Passwords]), UsersModule, AuthModule],
   controllers: [PasswordsController],
   providers: [PasswordsService],
 })
