1ba9fe673b332364f803a2e48eef943537ab46a7
Abdullah Omar <abdullah.aboode.omar@gmail.com>
2025-03-12 23:59:17 +0300

    added updatePassword handler

--- src/passwords/passwords.controller.ts
+++ src/passwords/passwords.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  UseGuards,
+  Req,
+  Patch,
+} from '@nestjs/common';
 import { PasswordsService } from './passwords.service';
 import { createPasswordDto } from './dtos/create-password.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { userRerquest } from 'src/passwords/interfaces/user-request.interface';
 
 @Controller('passwords')
 @UseGuards(AuthGuard)
@@ -9,8 +19,8 @@ export class PasswordsController {
   constructor(private readonly passwordService: PasswordsService) {}
 
   @Get()
-  async getAllPasswords(@Body('userid') userid: number) {
-    return await this.passwordService.getUserPasswords(userid);
+  async getAllPasswords(@Req() req: userRerquest) {
+    return await this.passwordService.getUserPasswords(req.users.userid);
   }
 
   @Post()
@@ -20,9 +30,25 @@ export class PasswordsController {
 
   @Delete()
   async deletePassword(
-    @Body('userid') userid: number,
+    @Req() req: userRerquest,
     @Body('siteName') siteName: string,
   ) {
-    return await this.passwordService.deletePassword(userid, siteName);
+    return await this.passwordService.deletePassword(
+      req.users.userid,
+      siteName,
+    );
+  }
+
+  @Patch()
+  async upadatePassword(
+    @Req() req: userRerquest,
+    @Body('siteName') siteName: string,
+    @Body('newPassword') newPassword: string,
+  ) {
+    return await this.passwordService.updatePassword(
+      req.users.userid,
+      siteName,
+      newPassword,
+    );
   }
 }
