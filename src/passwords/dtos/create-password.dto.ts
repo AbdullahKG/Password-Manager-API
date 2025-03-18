73108f988aba9f3946b4c02d6dbf6b19199ba042
Abdullah Omar <abdullah.aboode.omar@gmail.com>
2025-03-18 03:54:48 +0300

    deleted userid

--- src/passwords/dtos/create-password.dto.ts
+++ src/passwords/dtos/create-password.dto.ts
@@ -1,10 +1,6 @@
 import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class createPasswordDto {
-  @IsNumber()
-  @IsNotEmpty({ message: 'user id is required' })
-  userid: number;
-
   @IsString({ message: 'site should be provided' })
   @IsNotEmpty()
   siteName: string;
